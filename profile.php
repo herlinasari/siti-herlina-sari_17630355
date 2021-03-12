5acbd9db2e03bee186384a6804f697da4e47dafb
herlinasari <siti.helina.sari@gmail.com>
2021-03-13 00:35:04 +0800

    commit pertama: project praktikum Siti Herlina Sari - 17630355

---
+++ profile.php
@@ -0,0 +1,47 @@
+<?php include "header.php" ?>
+
+<div class="row">
+    <div class="col-md-12">
+        <div class="text-center">
+            <img src="img/17630355.jpg" alt="Siti Herlina Sari" width="160" class="rounded-circle">
+            <table class="table mt-4" style="width:100%; margin:0 auto;">
+                <tbody>
+                    <tr>
+                        <td align="right" width="50%"><strong>NIK / NIDN :</strong></td>
+                        <td align="left" width="50%">17630355</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Fakultas / Program Studi :</strong></td>
+                        <td align="left" width="50%">Teknologi Informasi / Teknik Informatika</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Nama Lengkap :</strong></td>
+                        <td align="left" width="50%">Siti Herlina Sari</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Tempat, Tanggal Lahir :</strong></td>
+                        <td align="left" width="50%">Banjarmasin, 19 Mei 1999</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Telepon :</strong></td>
+                        <td align="left" width="50%">0823 5065 1172</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>email :</strong></td>
+                        <td align="left" width="50%">siti.helina.sari@gmail.com</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Media Sosial :</strong></td>
+                        <td align="left" width="50%">
+                            <a href="http://instagram.com/linasariiii" target="_blank" style="text-decoration:none;">
+                                <img src="img/svg/029-instagram.svg" width="30" alt="instagram">
+                            </a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+
+<?php include "footer.php" ?>
